3a85073adba6df9d8ba948ab4ee18d6c3540b4ac
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2010-11-14 20:01:18 +0100

    improve qt-demobrowser start script

--- skel-root/bin/browser.sh
+++ skel-root/bin/browser.sh
@@ -2,6 +2,9 @@
 #
 # wrapper script to start the QT demo browser
 
-DFBARGS="cursor,linux-input-devices=/dev/event0,/dev/event1,/dev/event2"
-export DFBARGS
+modprobe evdev
+modprobe usbhid
+
+# hack, but I cannot pass it on the commandline...
+sed 's#linux-input-devices=.*#linux-input-devices=/dev/event0,/dev/event1#; s/no-cursor/cursor/;' /etc/directfbrc > /etc/directfbrc.browser
 /opt/qt/demos/browser/browser -qws -display directfb
