847cf1c312613fa7762c5f398abdcfc96a650d4b
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2011-01-06 21:52:56 +0100

    convert dropbear to .opk, installed in /opt/dropbear

--- make/extras.mk
+++ make/extras.mk
@@ -226,12 +226,15 @@ $(D)/dropbear: $(ARCHIVE)/dropbear-0.52.tar.bz2
 		$(BUILDENV) CFLAGS="$(TARGET_CFLAGS) -DDSS_PRIV_FILENAME=\"\\\"/opt/dropbear/etc/dropbear/dropbear_dss_host_key\\\"\" -DRSA_PRIV_FILENAME=\"\\\"/opt/dropbear/etc/dropbear/dropbear_rsa_host_key\\\"\"" \
 			 ./configure $(CONFIGURE_OPTS) --prefix=/opt/dropbear && \
 		$(MAKE) && \
-		$(MAKE) install DESTDIR=$(TARGETPREFIX)
-	install -D -m 0755 $(PATCHES)/dropbear.init $(TARGETPREFIX)/opt/dropbear/etc/init.d/dropbear
-	install -d -m 0755 $(TARGETPREFIX)/opt/dropbear/etc/dropbear
-	ln -sf dropbear $(TARGETPREFIX)/opt/dropbear/etc/init.d/S60dropbear
-	ln -sf dropbear $(TARGETPREFIX)/opt/dropbear/etc/init.d/K60dropbear
-	$(REMOVE)/dropbear-0.52
+		$(MAKE) install DESTDIR=$(PKGPREFIX)
+	install -D -m 0755 $(PATCHES)/dropbear.init $(PKGPREFIX)/opt/dropbear/etc/init.d/dropbear
+	install -d -m 0755 $(PKGPREFIX)/opt/dropbear/etc/dropbear
+	ln -sf dropbear $(PKGPREFIX)/opt/dropbear/etc/init.d/S60dropbear
+	ln -sf dropbear $(PKGPREFIX)/opt/dropbear/etc/init.d/K60dropbear
+	cp -a $(PKGPREFIX)/* $(TARGETPREFIX)
+	opkg.sh $(CONTROL_DIR)/dropbear $(TARGET) "$(MAINTAINER)" $(PKGPREFIX) $(BUILD_TMP)
+	mv $(PKGPREFIX)/dropbear-*.opk $(PACKAGE_DIR)
+	$(REMOVE)/dropbear-0.52 $(PKGPREFIX)
 	touch $@
 
 $(DEPDIR)/opkg: $(ARCHIVE)/opkg-0.1.8.tar.gz | $(TARGETPREFIX)
