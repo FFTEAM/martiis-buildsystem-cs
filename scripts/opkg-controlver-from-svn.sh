069e5190b7b543a8bb132de2c4b94096f14281ea
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2011-01-31 18:42:40 +0100

    cs-libs-pkg: fix generating of package version from SVN version

--- scripts/opkg-controlver-from-svn.sh
+++ scripts/opkg-controlver-from-svn.sh
@@ -3,7 +3,7 @@ CONTROL_FILE=$1
 shift
 SVNREF=0
 while test -n "$1"; do
-	R=$(svn info "$1" | awk -F: '/^Last Changed Rev:/{print $2}')
+	R=$(LC_ALL=C svn info "$1" | awk -F: '/^Last Changed Rev:/{print $2}')
 	test $SVNREF -lt $R && SVNREF=$R
 	shift
 done
