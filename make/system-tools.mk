f010832ccbc1d1554a778930da2a7f370fc5bf3f
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2011-02-26 14:40:27 +0100

    busybox: fix wget problem with patch from upstream

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -62,6 +62,7 @@ $(D)/busybox: $(ARCHIVE)/busybox-$(BUSYBOX-VER).tar.bz2 | $(TARGETPREFIX)
 	$(UNTAR)/busybox-$(BUSYBOX-VER).tar.bz2
 	rm -rf $(PKGPREFIX) $(BUILD_TMP)/bb-control
 	cd $(BUILD_TMP)/busybox-$(BUSYBOX-VER) && \
+		$(PATCH)/busybox-1.18.3-wget.patch && \
 		$(PATCH)/busybox-1.18.3-fix-libiproute-compile-with-kernel-headers-2.6.17.patch && \
 		$(PATCH)/busybox-1.18-hack-init-s-console.patch && \
 		cp $(PATCHES)/busybox-$(BUSYBOX-VER).config .config && \
