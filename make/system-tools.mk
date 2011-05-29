111f436fbec3ac317557ae6bc4f3be376dab971b
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2011-05-29 12:12:59 +0200

    busybox: add upstream setconsole fix

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -64,6 +64,7 @@ $(D)/busybox: $(ARCHIVE)/busybox-$(BUSYBOX-VER).tar.bz2 | $(TARGETPREFIX)
 	cd $(BUILD_TMP)/busybox-$(BUSYBOX-VER) && \
 		$(PATCH)/busybox-1.18.3-fix-libiproute-compile-with-kernel-headers-2.6.17.patch && \
 		$(PATCH)/busybox-1.18-hack-init-s-console.patch && \
+		$(PATCH)/busybox-1.18-setconsole-open-console-for-writing-rather-than-read.patch && \
 		cp $(PATCHES)/busybox-1.18.config .config && \
 		sed -i -e 's#^CONFIG_PREFIX.*#CONFIG_PREFIX="$(PKGPREFIX)"#' .config && \
 		grep -q DBB_BT=AUTOCONF_TIMESTAMP Makefile.flags && \
