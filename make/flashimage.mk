adfa7fd057cbace078421e32c8e33f37e3470bf9
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2011-06-08 23:12:51 +0200

    clarify the supported target for the jffs2 image

--- make/flashimage.mk
+++ make/flashimage.mk
@@ -11,11 +11,16 @@ NOW := $(shell date +%Y%m%d%H%M)
 FLASHIMG = $(BUILD_TMP)/flashroot-$(NOW).img
 SUMIMG   = $(BUILD_TMP)/flashroot-$(NOW).sum.img
 
+ifeq ($(PLATFORM), coolstream)
 # the devtable is used for having a console device on first boot.
 flashimage: find-mkfs.jffs2 find-sumtool
 	echo "/dev/console c 0644 0 0 5 1 0 0 0" > $(BUILD_TMP)/devtable
 	mkfs.jffs2 -e 0x20000 -p -U -D $(BUILD_TMP)/devtable -d $(BUILD_TMP)/install -o $(FLASHIMG)
 	sumtool    -e 0x20000 -i $(FLASHIMG) -o $(SUMIMG)
+else
+flashimage:
+	@echo flashimage is not a supported target for $(PLATFORM)
+endif
 
 mtd-utils: $(ARCHIVE)/mtd-utils-1.4.4.tar.bz2 | $(HOSTPREFIX)/bin
 	$(UNTAR)/mtd-utils-1.4.4.tar.bz2
