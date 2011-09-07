aeeeb9c21564ace456d9ba0fa6d8c31d0c05214c
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2011-09-07 14:15:57 +0200

    fix libpng download url

--- make/archives.mk
+++ make/archives.mk
@@ -48,7 +48,7 @@ $(ARCHIVE)/curl-$(CURL-VER).tar.bz2:
 	$(WGET) http://curl.haxx.se/download/curl-$(CURL-VER).tar.bz2
 
 $(ARCHIVE)/libpng-$(PNG-VER).tar.bz2:
-	$(WGET) http://downloads.sourceforge.net/project/libpng/libpng12/$(PNG-VER)/libpng-$(PNG-VER).tar.bz2
+	$(WGET) http://downloads.sourceforge.net/libpng/libpng-$(PNG-VER).tar.bz2
 
 $(ARCHIVE)/dropbear-$(DROPBEAR-VER).tar.bz2:
 	$(WGET) http://matt.ucc.asn.au/dropbear/releases/dropbear-$(DROPBEAR-VER).tar.bz2
