44d569c2362f3f093cd9e0bc3cbdebfcf465a158
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2012-04-06 21:33:22 +0200

    add "flashimage" target for SPARK

--- make/flashimage.mk
+++ make/flashimage.mk
@@ -17,7 +17,28 @@ flashimage: find-mkfs.jffs2 find-sumtool
 	echo "/dev/console c 0644 0 0 5 1 0 0 0" > $(BUILD_TMP)/devtable
 	mkfs.jffs2 -e 0x20000 -p -U -D $(BUILD_TMP)/devtable -d $(BUILD_TMP)/install -o $(FLASHIMG)
 	sumtool    -e 0x20000 -i $(FLASHIMG) -o $(SUMIMG)
-else
+endif
+ifeq ($(PLATFORM), spark)
+# you should probably "make system-pkgs" before...
+# this has been tested by flashing from an USB stick on GM 990
+flashimage: find-mkfs.jffs2 find-sumtool
+	# copy local/flash/* into the image...
+	# you can e.g. create local/flash/boot/audio.elf ...
+	@if test -d $(BASE_DIR)/local/flash/; then \
+		cp -a $(BASE_DIR)/local/flash/. $(BUILD_TMP)/install/.; \
+	fi
+	echo "/dev/console c 0644 0 0 5 1 0 0 0" > $(BUILD_TMP)/devtable
+	ln -sf /share/zoneinfo/CET $(BUILD_TMP)/install/etc/localtime # CET is the default in a fresh neutrino install
+	mkfs.jffs2 -e 0x20000 -p -U -D $(BUILD_TMP)/devtable -d $(BUILD_TMP)/install -o $(FLASHIMG)
+	sumtool    -e 0x20000 -i $(FLASHIMG) -o $(SUMIMG)
+	@set -e; rm -rf $(BUILD_TMP)/enigma2; mkdir $(BUILD_TMP)/enigma2; \
+		cd $(BUILD_TMP)/enigma2; \
+		cp -a $(BUILD_TMP)/uImage .; \
+		cp -a $(SUMIMG) e2jffs2.img; \
+		echo; echo; echo "SPARK flash image is in build_tmp/enigma2:"; ls -l *; \
+		echo; echo "copy this directory onto an USB stick and flash via the boot loader.";
+endif
+ifeq ($(PLATFORM), tripledragon)
 flashimage:
 	@echo flashimage is not a supported target for $(PLATFORM)
 endif
