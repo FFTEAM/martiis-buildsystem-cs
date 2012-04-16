a613b9b662fc14057ad9fa8158038adb9b81656b
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2012-04-16 09:43:59 +0200

    cross-pkg-config: check prerequisites, remove old option
    the update to pkg-config 0.26 removed the embedded glib, so
    now glib-2.0 devel packages are needed on the build host

--- make/bootstrap.mk
+++ make/bootstrap.mk
@@ -339,15 +339,20 @@ $(TARGETPREFIX)/sbin/ldconfig: | $(TARGETPREFIX)
 		echo "====================================================="; \
 	fi
 
+pkg-config-preqs:
+	@PATH=$(subst $(HOSTPREFIX)/bin:,,$(PATH)); \
+		if ! pkg-config --exists glib-2.0; then \
+			echo "pkg-config and glib2-devel packages are needed for building cross-pkg-config."; false; \
+		fi
+
 pkg-config: $(HOSTPREFIX)/bin/pkg-config
-$(HOSTPREFIX)/bin/pkg-config: $(ARCHIVE)/pkg-config-$(PKGCONFIG_VER).tar.gz | $(HOSTPREFIX)/bin
+$(HOSTPREFIX)/bin/pkg-config: $(ARCHIVE)/pkg-config-$(PKGCONFIG_VER).tar.gz | $(HOSTPREFIX)/bin pkg-config-preqs
 	$(UNTAR)/pkg-config-$(PKGCONFIG_VER).tar.gz
-	cd $(BUILD_TMP)/pkg-config-$(PKGCONFIG_VER) && \
-		./configure --with-pc_path=$(PKG_CONFIG_PATH) \
-					--with-installed-glib && \
-		$(MAKE) && \
+	set -e; cd $(BUILD_TMP)/pkg-config-$(PKGCONFIG_VER); \
+		./configure --with-pc_path=$(PKG_CONFIG_PATH); \
+		$(MAKE); \
 		cp -a pkg-config $(HOSTPREFIX)/bin
-	ln -s pkg-config $(HOSTPREFIX)/bin/$(TARGET)-pkg-config
+	ln -sf pkg-config $(HOSTPREFIX)/bin/$(TARGET)-pkg-config
 
 # hack to make sure they are always copied
 PHONY += $(TARGETPREFIX)/lib/modules/2.6.26.8-nevis
