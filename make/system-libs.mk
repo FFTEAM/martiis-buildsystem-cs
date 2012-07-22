0fd942cb6c80e529a70fb12d113633f847c417d7
martii <you@example.com>
2012-07-22 16:03:57 +0200

    system-libs.mk: remove no longer used ffmpeg patch

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -278,7 +278,6 @@ else
 endif
 	set -e; cd $(BUILD_TMP)/ffmpeg-$(FFMPEG_VER); \
 		: $(PATCH)/ffmpeg-dvbsubs.diff; \
-		echo $(PATCH)/ffmpeg-0.6-avoid-UINT64_C.diff; \
 		$(PATCH)/ffmpeg-0.10-remove-buildtime.diff; \
 		./configure \
 			--disable-encoders \
