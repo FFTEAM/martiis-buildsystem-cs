83b9e8bf5dc2e93cf04ae24db72b9bc6b2a0c1c4
martii <you@example.com>
2012-08-19 12:22:06 +0200

    mdev-usbcontrol fix

--- skel-root/spark/etc/mdev/mdev-usbcontrol.sh
+++ skel-root/spark/etc/mdev/mdev-usbcontrol.sh
@@ -2,8 +2,18 @@
 IFS=/
 set $PRODUCT
 IFS=
-VENDORID=`printf "%.4x" $1`
-PRODUCTID=`printf "%.4x" $2`
+VENDORID=`printf "%.4x" 0x$1`
+PRODUCTID=`printf "%.4x" 0x$2`
 C="/share/usb_modeswitch/$VENDORID:$PRODUCTID"
-[ -f "$C" ] && usb_modeswitch -v $VENDORID -p $PRODUCTID -c "$C"
+if [ -f "$C" ]
+then
+	usb_modeswitch -v $VENDORID -p $PRODUCTID -c "$C"
+	exit 0;
+fi
+case "$TYPE" in
+2/*)
+	rmmod usbserial
+	modprobe usbserial vendor=0x$VENDORID product=0x$PRODUCTID
+	;;
+esac
 
