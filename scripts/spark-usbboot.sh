99701f74d377ddc787d409e63b8b5d7b62196ff1
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2013-04-21 14:58:16 +0200

    spark-usbboot: use ext3 instead of ext2 for root fs
    If the USB stick is once plugged into a PC and mounted as ext3, it will
    no longer mount as ext2 due to "unsupported features" in the filesystem.
    In order to avoid nasty surprises, use ext3 for the root fs. Testing
    shows that there are no differences in performance.

--- scripts/spark-usbboot.sh
+++ scripts/spark-usbboot.sh
@@ -52,7 +52,7 @@ fatload usb 0:1 80000000 uImage-7162
 else
 fatload usb 0:1 80000000 uImage
 fi
-setenv bootargs console=ttyAS0,115200 root=/dev/sda2 rootfstype=ext2 rw coprocessor_mem=4m@0x40000000,4m@0x40400000 printk=1 printk.time=1 nwhwconf=device:eth0,hwaddr:00:80:E1:12:40:69 bigphysarea=6000 stmmaceth=msglvl:0,phyaddr:2,watchdog:5000 panic=10 rootwait usb_storage.delay_use=0
+setenv bootargs console=ttyAS0,115200 root=/dev/sda2 rootfstype=ext3 rw coprocessor_mem=4m@0x40000000,4m@0x40400000 printk=1 printk.time=1 nwhwconf=device:eth0,hwaddr:00:80:E1:12:40:69 bigphysarea=6000 stmmaceth=msglvl:0,phyaddr:2,watchdog:5000 panic=10 rootwait usb_storage.delay_use=0
 bootm 80000000
 EOF
 
