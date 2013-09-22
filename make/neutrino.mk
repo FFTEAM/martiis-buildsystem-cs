136f0ecad949c5577b6cb19fc040ba57f7f7479d
martii <m4rtii@gmx.de>
2013-09-22 23:38:52 +0200

    Revert "make/neutrino.mk: drop -DMARTII". Still needed for plugins.
    This reverts commit 38091b76628dc14e4e59664c0f5c55d01abff77b.

--- make/neutrino.mk
+++ make/neutrino.mk
@@ -5,7 +5,7 @@ NEUTRINO_DEPS += openthreads
 NEUTRINO_DEPS += lua
 NEUTRINO_PKG_DEPS =
 
-N_CFLAGS   = -Wall -W -Wshadow -g -O2 -fno-strict-aliasing -rdynamic -DNEW_LIBCURL -DCPU_FREQ $(LOCAL_NEUTRINO_CFLAGS)
+N_CFLAGS   = -Wall -W -Wshadow -g -O2 -fno-strict-aliasing -rdynamic -DNEW_LIBCURL -DCPU_FREQ -DMARTII $(LOCAL_NEUTRINO_CFLAGS)
 N_CPPFLAGS = -I$(TARGETPREFIX)/include
 ifeq ($(PLATFORM), coolstream)
 N_CPPFLAGS += -DUSE_NEVIS_GXA
