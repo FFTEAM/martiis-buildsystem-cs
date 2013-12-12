b6677b09d0521636954599515da5cc0a843e5661
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2013-12-12 14:57:24 +0100

    fbshot: add missing zlib include for new libpng

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -332,11 +332,13 @@ $(D)/unfsd: $(D)/libflex $(D)/portmap $(ARCHIVE)/unfs3-$(UNFS3-VER).tar.gz
 	touch $@
 
 fbshot: $(TARGETPREFIX)/bin/fbshot
-$(TARGETPREFIX)/bin/fbshot: $(ARCHIVE)/fbshot-$(FBSHOT-VER).tar.gz $(PATCHES)/fbshot-0.3-32bit_cs_fb.diff $(PATCHES)/fbshot-0.3-32bit_td_fb.diff libpng | $(TARGETPREFIX)
+$(TARGETPREFIX)/bin/fbshot: $(ARCHIVE)/fbshot-$(FBSHOT-VER).tar.gz $(wildcard $(PATCHES)/fbshot-0.3-*.diff) libpng | $(TARGETPREFIX)
 	$(UNTAR)/fbshot-$(FBSHOT-VER).tar.gz
 	set -e; cd $(BUILD_TMP)/fbshot-$(FBSHOT-VER); \
 		$(PATCH)/fbshot-0.3-32bit_cs_fb.diff; \
 		$(PATCH)/fbshot-0.3-32bit_td_fb.diff; \
+		$(PATCH)/fbshot-0.3-libpng16.diff
+	set -e; cd $(BUILD_TMP)/fbshot-$(FBSHOT-VER); \
 		$(TARGET)-gcc -DHW_$(PLATFORM) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) fbshot.c -lpng -lz -o $@
 	$(REMOVE)/fbshot-$(FBSHOT-VER)
 
