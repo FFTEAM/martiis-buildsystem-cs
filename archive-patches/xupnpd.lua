ab6ceb268ca06baa43a1d6c35f17b3066a139362
martii <m4rtii@gmx.de>
2013-12-17 14:52:50 +0100

    archive-patches/xupnpd.lua: remove already deactivated code

--- archive-patches/xupnpd.lua
+++ archive-patches/xupnpd.lua
@@ -115,7 +115,4 @@ cfg.ui_path='./ui/'
 cfg.drive=''                    -- reload playlists only if drive state=active/idle, example: cfg.drive='/dev/sda'
 cfg.profiles='./profiles/'      -- device profiles feature
 
---file = io.open("/tmp/.timer_wakeup", "w")
---file:close()
-
 dofile('xupnpd_main.lua')
